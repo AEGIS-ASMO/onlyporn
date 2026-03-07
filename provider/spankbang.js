ad1a6031605dfafa4822bd071bde02fc2c401599
AEGIS-ASMO <kartikayshukla864@gmail.com>
2026-03-07 16:42:48 +0530

    Update spankbang.js

--- provider/spankbang.js
+++ provider/spankbang.js
@@ -1,160 +1,78 @@
-const { load } = require('cheerio');
-const logger = require('../logger');
-const { meta } = require('../model');
-const Provider = require('./provider');
+const Provider = require('../provider')
+const cheerio = require('cheerio')
 
-const pathMappings = {
-  'Trending': '/trending_videos/',
-  'New': '/new_videos/',
-  'Popular': '/most_popular/',
-  'Upcoming': '/upcoming/',
-};
-
-class SpankbangProvider extends Provider {
+class Spankbang extends Provider {
 
   constructor() {
-    super('https://spankbang.com', 'spankbang', 80);
+    super('https://spankbang.com', 'spankbang', 10)
   }
 
-  static create() {
-    return new SpankbangProvider();
-  }
+  async search(query) {
 
-  getInitialUrl(catalogId) {
-    return this.baseUrl + pathMappings.Trending;
-  }
+    const url = `${this.baseUrl}/s/${encodeURIComponent(query)}/`
+    const html = await this.request(url)
 
-  handleSearch({ extra: { search: keyword } }) {
-    return `${this.baseUrl}/s/${keyword}/`;
-  }
-  async fetchHtml(url) {
-    logger.info({ url }, 'fetching url');
-    try {
-      const response = await fetch(url, {
-        "headers": {
-          "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-          "accept-language": "en-US,en;q=0.9",
-          "priority": "u=0, i",
-          "sec-ch-ua": "\"Chromium\";v=\"124\", \"Google Chrome\";v=\"124\", \"Not-A.Brand\";v=\"99\"",
-          "sec-ch-ua-mobile": "?0",
-          "sec-ch-ua-platform": "\"macOS\"",
-          "sec-fetch-dest": "document",
-          "sec-fetch-mode": "navigate",
-          "sec-fetch-site": "same-origin",
-          "sec-fetch-user": "?1",
-          "upgrade-insecure-requests": "1"
-        },
-        "referrer": "https://spankbang.com/9hj5s/video/teen+with+big+natural+tits+was+fucked+in+the+forest+in+doggy+position",
-        "referrerPolicy": "strict-origin-when-cross-origin",
-        "body": null,
-        "method": "GET",
-        "mode": "cors",
-        "credentials": "include"
-      });
-      const r = await response.text();
-      return r;
-    } catch (error) {
-      console.error(error.message);
-      return '';
-    }
-  }
+    const $ = cheerio.load(html)
+    const results = []
 
-  handleGenre({ extra: { genre } }) {
-    const [keyword, order] = genre.split('(');
-    if (order) {
-      const searchUrl = this.handleSearch({ extra: { search: encodeURIComponent(keyword.trim()) } });
-      return searchUrl + `?o=${order.replace(')', '').toLowerCase()}`;
-    }
+    $('div.video-item, div.thumb').each((i, el) => {
 
-    const path = pathMappings[keyword] || pathMappings.New;
-    return `${this.baseUrl}${path}`;
-  }
+      const node = $(el)
 
-  handlePagination(url, { extra: { skip } }) {
-    const prefix = url.lastIndexOf('/') === 7 ? '/' : '';
-    return `${prefix}${this.page(skip)}/`;
-  }
+      const link = node.find('a').attr('href')
+      if (!link) return
 
-  getCatalogMetas(html) {
-    const metadataList = [];
-    const $ = load(html);
+      const id = link.split('/')[1]
 
-    $('div.video-item')
-      .each((index, element) => {
-        const $e = $(element);
-        const id = $e.attr('data-id');
-        const $children = $e.children();
-        const $first = $children.first();
-        const $imgNode = $first.children().children('img');
-        const poster = $imgNode.attr('data-src');
-        const title = $imgNode.attr('alt');
-        const videoPageUrl = this.baseUrl + $first.attr('href');
+      const title =
+        node.find('a').attr('title') ||
+        node.find('.name').text() ||
+        id
 
-        if (id) {
-          metadataList.push(new meta.MetaPreview(videoPageUrl, 'movie', title, poster, {
-            videoPageUrl,
-          }));
-        }
-      });
+      const img = node.find('img')
 
-    return metadataList;
-  }
+      const poster =
+        img.attr('data-src') ||
+        img.attr('src') ||
+        ''
 
-  async getMetadata(args) {
-    logger.debug({ args }, 'getMetadata');
-    const { id } = args;
-    return this.fetchHtml(id)
-      .then(html => this.parseVideoPage({ id, html }))
-      .catch((error) => {
-        logger.error({ error, args }, 'getMetadata error');
-        throw error;
+      results.push({
+        id,
+        name: title.trim(),
+        poster,
+        type: 'movie'
       })
+
+    })
+
+    return results
   }
 
-  parseVideoPage({ id, html }) {
-    const $ = load(html);
-    const $scripts = $('script');
-    const STREAM_DATA = 'stream_data';
-    const VIDEO_OBJECT = 'VideoObject';
-    const datas = $scripts.filter((i, e) => e.children.length !== 0)
-      .map((i, e) => e.children[0].data)
-      .filter((i, data) => {
-        return data.indexOf(STREAM_DATA) !== -1
-          || data.indexOf(VIDEO_OBJECT) !== -1;
-      });
-    const url = $('meta[property="og:url"]')[0].attribs.content;
-    const regex = /'m3u8':\s*\['([^\]]+)'\],/;
-    let match = datas[0].match(regex);
-    if (match && match[1]) {
-      match = match[1];
+  async load(id) {
+
+    const url = `${this.baseUrl}/${id}/video`
+    const html = await this.request(url)
+
+    const streams = []
+
+    const match = html.match(/https:\/\/[^"]+\.m3u8/g)
+
+    if (match) {
+
+      match.forEach(stream => {
+
+        streams.push({
+          title: 'SpankBang',
+          url: stream
+        })
+
+      })
+
     }
-    const {
-      name,
-      thumbnailUrl,
-      description,
-      keywords,
-    } = JSON.parse(datas[1].replace(/[\r\n\t]/g, ''));
 
-    return new meta.MetaResponse(
-      url,
-      'movie',
-      name,
-      {
-        videoPageUrl: match,
-        poster: thumbnailUrl,
-        genres: keywords.split(','),
-        background: thumbnailUrl,
-        description,
-      },
-    );
+    return streams
   }
+
 }
 
-const prov = new SpankbangProvider();
-// prov.handleCatalog({type: 'movie', extra: {}})
-// prov.handleStream({
-//     type: 'movie',
-//     id: '/9gj7a/video/blacked+anal+hungry+gabbie+carter+craves+anton+s+huge+bbc'
-// })
-
-module.exports = SpankbangProvider.create;
+module.exports = Spankbang
